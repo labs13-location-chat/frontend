b0a1cfa0e201314d3e8124f8e3bf617fa656f928
jvgiv <jvgiv12@gmail.com>
2019-06-14 15:17:15 -0400

    backing up index and app

---
+++ AppBackup.js
@@ -0,0 +1,13 @@
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+import LocalChat from './src/components/index';
+
+export default class App extends Component {
+  render() {
+    return (
+        <LocalChat />
+    );
+  }
+}
