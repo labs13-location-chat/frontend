5992e3283ac37371d9a5e7eacab3aa4cb0ff0187
jvgiv <jvgiv12@gmail.com>
2019-06-17 17:37:44 -0400

    Sendbird initializer added to App.js

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import Sendbird from 'sendbird'
+import Config from './src/config'
 import LocalChat from './src/components/index';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    // Initialize sendbird using our app id
+    const sb = new Sendbird({ appId: Config.appId })
+  }
+  
   render() {
     return (
         <LocalChat />
