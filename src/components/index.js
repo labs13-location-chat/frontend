df7200d72db54fffac74bdde536abef5f6bc308a
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-06-18 12:58:28 -0700

    chatroom map rendering dynamic location of chatroom

--- src/components/index.js
+++ src/components/index.js
@@ -12,6 +12,7 @@ import Notifications from './BottomTabs/Notifications';
 import ChatApp from '../components/chatroom/twilio/ChatApp';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { AsyncStorage, View, Text } from 'react-native';
+import MessageRoom from './chatroom/messages/MessageRoom';
 import {
 	createAppContainer,
 	createStackNavigator,
@@ -48,6 +49,7 @@ const Settings = createStackNavigator({
 
 const JoinChats = createStackNavigator({
     'JoinChat': { screen: JoinChat },
+    "Chatroom" : { screen: MessageRoom }
     },
         {
         navigationOptions: {
