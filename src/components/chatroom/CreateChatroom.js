93fc21130e514a8e792a245670dd0967b3553938
John Gibbons <jvgiv12@gmail.com>
2019-06-26 13:26:32 -0400

    CreateChatroom page added

---
+++ src/components/chatroom/CreateChatroom.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {
+    View,
+    StyleSheet,  
+    Text,
+    Button,
+    Dimensions,
+    TouchableOpacity
+  } from "react-native";
+
+export default class CreateChatroom extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
