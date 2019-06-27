439fd390bd76199550228b46496faf3647698dcf
DaytonS3 <Daytonrsteinmeyer@gmail.com>
2019-06-27 13:28:24 -0400

    setup redux

--- index.js
+++ index.js
@@ -2,9 +2,21 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import React from "react";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { Provider } from "react-redux";
+import configureStore from "./redux/store";
+
+const store = configureStore();
 
 console.disableYellowBox = true;
-AppRegistry.registerComponent(appName, () => App);
+
+const RNRedux = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => RNRedux);
---
+++ redux/reducers/reducer.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+let initialState = {};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
